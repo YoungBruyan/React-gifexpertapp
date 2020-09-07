0bdea021abd31c447e464c4038c8f7d6e3739494
Daniel Celis <bryan_dca@hotmail.com>
2020-09-06 22:22:00 -0500

    docs agregado

---
+++ docs/precache-manifest.4eea7273809e5b53425d8be722ef65bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f93654f56da05b88bf344642a13f73a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ec16a9c9645ddeb3652",
+    "url": "/static/css/main.4efaf335.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ec16a9c9645ddeb3652",
+    "url": "/static/js/main.23c40c1b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
